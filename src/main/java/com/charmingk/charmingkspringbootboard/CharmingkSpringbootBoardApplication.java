a3a549fb3278378ae6d3f6cebf04aa91360bc09e
developer <blue007m@naver.com>
2021-10-01 17:04:10 +0900

    Initial commit

---
+++ src/main/java/com/charmingk/charmingkspringbootboard/CharmingkSpringbootBoardApplication.java
@@ -0,0 +1,13 @@
+package com.charmingk.charmingkspringbootboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharmingkSpringbootBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharmingkSpringbootBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charmingk/charmingkspringbootboard/CharmingkSpringbootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charmingk.charmingkspringbootboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharmingkSpringbootBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
