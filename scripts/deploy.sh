bb5b0c94cb1e930bc915fb5c9740a26212ef8f55
developer <blue007m@naver.com>
2021-10-12 14:52:27 +0900

    배포 자동화 설정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,8 @@ PROJECT_NAME=charmingk-springboot-board
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo ">현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -f ${PROJECT_NAME} | grep jar | awk '{print $1}')
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
@@ -27,6 +27,6 @@ echo "> $JAR_NAME 실행"
 
 nohup java -jar \
         -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-        -Dspring.profiles.active=real \
+         -Dspring.profiles.active=real \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
